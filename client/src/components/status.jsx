3d5a9300e9d0df817a02661637bc6c29d65602da
santamariamoises <santamaria.moises@gmail.com>
2018-10-11 18:34:05 -0500

    staus almost working

--- client/src/components/status.jsx
+++ client/src/components/status.jsx
@@ -1,16 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react';
+//import "./Styles.css";
+import { ProgressBar, Step } from "react-step-progress-bar";
 
-// this is going to retrieve info from database,
-//indicating the status of the order
+class StepProgressBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }
 
-
-//if orden en curso, render (conditional rendering)
-const Status = (props) => {
- return (
-   <div>
-     <h2>Tu ropa está **{props.status}**</h2>
-  </div>
- );
+render(){
+  return (
+   <ProgressBar
+        percent={75}
+        filledBackground="linear-gradient(to right, #fefb72, #f0bb31)"
+      >
+        <Step transition="scale">
+          {({ accomplished }) => (
+            <img
+              style={{ filter: `grayscale(${accomplished ? 0 : 80}%)` }}
+              width="30"
+              src="https://vignette.wikia.nocookie.net/pkmnshuffle/images/9/9d/Pichu.png/revision/latest?cb=20170407222851"
+            />
+          )}
+        </Step>
+        <Step transition="scale">
+          {({ accomplished }) => (
+            <img
+              style={{ filter: `grayscale(${accomplished ? 0 : 80}%)` }}
+              width="30"
+              src="https://vignette.wikia.nocookie.net/pkmnshuffle/images/9/97/Pikachu_%28Smiling%29.png/revision/latest?cb=20170410234508"
+            />
+          )}
+        </Step>
+        <Step transition="scale">
+          {({ accomplished }) => (
+            <img
+              style={{ filter: `grayscale(${accomplished ? 0 : 80}%)` }}
+              width="30"
+              src="https://orig00.deviantart.net/493a/f/2017/095/5/4/raichu_icon_by_pokemonshuffle_icons-db4ryym.png"
+            />
+          )}
+        </Step>
+        <Step transition="scale">
+          {({ accomplished }) => (
+            <img
+              style={{ filter: `grayscale(${accomplished ? 0 : 80}%)` }}
+              width="30"
+              src="https://orig00.deviantart.net/493a/f/2017/095/5/4/raichu_icon_by_pokemonshuffle_icons-db4ryym.png"
+            />
+          )}
+        </Step>
+        <Step transition="scale">
+          {({ accomplished }) => (
+            <img
+              style={{ filter: `grayscale(${accomplished ? 0 : 80}%)` }}
+              width="30"
+              src="https://orig00.deviantart.net/493a/f/2017/095/5/4/raichu_icon_by_pokemonshuffle_icons-db4ryym.png"
+            />
+          )}
+        </Step>
+      </ProgressBar>
+    );
+  }
 }
-
-export default Status;
+export default StepProgressBar;

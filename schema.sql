39b59b1b440bca30a5aa12a9dfeb1973384129e6
Usuario <dsanchez0522@gmail.com>
2018-10-16 12:22:05 -0500

    rebasing

--- schema.sql
+++ schema.sql
@@ -9,23 +9,24 @@ CREATE TABLE orders(
   name varchar(50) NOT NULL,
   lastname varchar(50) NOT NULL,
   address varchar(100) NOT NULL,
-  email varchar(50) NOT NULL,
-  phone varchar(10) NOT NULL,
-  status varchar(10) NOT NULL,
-  PRIMARY KEY (ID),
+  email varchar(50) NOT NULL PRIMARY KEY,
+  phone varchar(10) NOT NUL
+
 );
 
 
-CREATE TABLE users(
+
+CREATE TABLE status(
   id int NOT NULL AUTO_INCREMENT,
-  mail varchar(50) NOT NULL,
-  userName varchar(50) NOT NULL,
-  PRIMARY KEY (ID)
-);
+  email varchar (50) NOT NULL,
+  userName varchar (50) NOT NULL,
+  PRIMARY KEY (ID),
+  FOREIGN KEY (email) REFERENCES email(email)
 
+);
 
 /*  Execute this file from the command line by typing:
- *    mysql -u root -p < schema.sql
+ *    mysql -u root < schema.sql
  *  to create the database and the tables.*/
 
- INSERT INTO users (mail, userName) VALUES ("jazzcelaya@gmail.com", "Jazz");
+INSERT INTO orders  (name, lastname, address, email, phone) VALUES("John", "Lopez", "77 toledo", "jolo@gmail.com", "5573589699");
